e46595731f9e540836a7e846655e0642a3ba5047
Laura Hope <ssteamlaura@gmail.com>
2023-04-03 09:27:12 -0700

    Add initial HTML template, js/scripts.js user interface information, and empty README.md and styles.css files.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+
+
+
+// User Inferface Logic
+
+function handleSubmission(event){
+  event.preventDefault();
+  const firstName
+  const lastName
+  const phoneNumber
+}
+
+window.addEventListener("load", function(){
+  const form = document.getEventById("addressBook");
+  form.addEventListener("submit", handleSubmission);
+})
